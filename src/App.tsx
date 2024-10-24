957b82838886cc3502c6520f0dd96e00401f8aee
Carlos Alexandre <alexfilho062005@gmail.com>
2024-10-24 14:14:46 -0300

    feat: Component Button and section input finished styled

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
+import ButtonCreate from './components/ButtonCreate';
 import { Header } from './components/Header';
 import Input from './components/Input';
 
 function App() {
   return (
-    <main className="h-screen w-full bg-gray-600 font-inter leading-line ">
+    <main className="h-screen bg-gray-600 font-inter leading-line ">
       <Header />
 
-      <div className="flex gap-2 mx-auto -mt-8">
+      <div className="max-w-[736px] flex justify-center px-3 gap-2 mx-auto -mt-8">
         <Input />
+        <ButtonCreate />
       </div>
     </main>
   );
---
+++ src/components/ButtonCreate.tsx
@@ -0,0 +1,8 @@
+export default function ButtonCreate() {
+  return (
+    <button className="flex items-center gap-2 bg-blue-dark font-bold text-sm text-gray-100 p-4 rounded-lg hover:bg-blue duration-300">
+      Criar
+      <img src="plus.svg" alt="plus" />
+    </button>
+  );
+}
