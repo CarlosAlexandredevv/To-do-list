8fe65d518f102653f00762d0d82c9c074dc74ff4
Carlos Alexandre <alexfilho062005@gmail.com>
2024-10-24 13:27:06 -0300

    Feat::Input component Styled

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import { Header } from './components/Header';
+import Input from './components/Input';
 
 function App() {
   return (
-    <main>
+    <main className="h-screen w-full bg-gray-600 font-inter leading-line ">
       <Header />
+
+      <div className="flex gap-2 mx-auto -mt-8">
+        <Input />
+      </div>
     </main>
   );
 }
---
+++ src/components/Input.tsx
@@ -0,0 +1,8 @@
+export default function Input() {
+  return (
+    <input
+      className="bg-gray-500 text-gray-100 p-4 border-2 border-gray-700 rounded-lg placeholder:text-gray-300 focus:border-purple-dark outline-none "
+      placeholder="Adicione uma nova tarefa"
+    />
+  );
+}
