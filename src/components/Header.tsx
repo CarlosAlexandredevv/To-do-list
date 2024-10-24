244f8d4360717f36a33f35c8e9445cd64aeda64f
Carlos Alexandre <alexfilho062005@gmail.com>
2024-10-24 12:20:56 -0300

    feat: header finished:

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export function Header() {
+  return (
+    <header className="flex items-center justify-center h-[200px] bg-gray-700">
+      <img src="Logo.svg" alt="Logo" />
+    </header>
+  );
+}
