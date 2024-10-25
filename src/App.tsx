6400dc378edd17848dd955033cca8f777080d418
Carlos Alexandre <alexfilho062005@gmail.com>
2024-10-25 18:00:32 -0300

    feat: styles task organizeed

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
           <ButtonCreate />
         </div>
 
-        <div className="flex justify-between mt-16 pb-6 border-b border-gray-400">
+        <div className="flex justify-between mt-16">
           <h2 className="flex items-center gap-2 text-blue text-sm font-bold">
             Tarefas criadas
             <span className="text-gray-200 bg-gray-400 px-2 py-[2px] rounded-full text-xs">
@@ -26,6 +26,18 @@ function App() {
               0
             </span>
           </h2>
+        </div>
+
+        <div className="h-[286px] flex flex-col gap-4 justify-center items-center mt-6 rounded-lg border-t-2 border-gray-400">
+          <img src="Clipboard.svg" alt="Clipboard" />
+          <div className="text-center">
+            <h3 className="text-gray-300 font-bold">
+              Você ainda não tem tarefas cadastradas
+            </h3>
+            <span className="text-gray-300">
+              Crie tarefas e organize seus itens a fazer
+            </span>
+          </div>
         </div>
       </div>
     </main>
