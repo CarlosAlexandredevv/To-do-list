e24556f1b3f9a203fcf52fc0b83a461c8d2599f2
Carlos Alexandre <alexfilho062005@gmail.com>
2024-11-05 17:37:27 -0300

    bug task text

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -31,15 +31,16 @@ export default function Task({
   }
 
   return (
-    <div className="flex bg-gray-500 border border-gray-400 rounded-lg p-4 truncate">
+    <div className="flex bg-gray-500 border border-gray-400 rounded-lg p-4">
       <Checkbox onClick={handleCheckboxChange} />
       <p
-        className={`mx-3 flex-1 text-sm font-inter truncate ${
+        className={`mx-3 flex-1 text-sm font-inter break-words ${
           isChecked ? 'text-gray-300 line-through' : 'text-gray-100'
         }`}
       >
         {text}
       </p>
+
       <ButtonTrash onClick={handleDeleteTask} />
     </div>
   );
